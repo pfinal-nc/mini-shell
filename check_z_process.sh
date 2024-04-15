d618991aba35cec57a4f5c1536377a25c0dc0f6a
PFinal_南丞 <1370288056@qq.com>
2024-04-15 11:17:26 +0800

    更新

--- check_z_process.sh
+++ check_z_process.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+os_type=$(uname -s)
 
-#查看僵尸进程
-ps -al | gawk '{print $2,$4}' | grep Z
+echo "Running script with OS type: $os_type"
+
+if [ "$os_type" = "CYGWIN" ] || [ "$os_type" = "MINGW" ] || [ "$os_type" = "Darwin" ]; then
+    echo "Using ps -al | awk '{print $2,$4}' | grep Z"
+    ps -al | awk '{print $2,$4}' | grep Z
+else
+    echo "Using ps -al | gawk '{print $2,$4}' | grep Z"
+    ps -al | gawk '{print $2,$4}' | grep Z
+fi
+echo "Done running the script"
+
+
